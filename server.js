dfa75356293c0fbf2a5917f565623141cc1fa96c
Hearsaytoken <126023881+Hearsaytoken@users.noreply.github.com>
2024-06-20 08:27:51 -0400

    yay

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ app.use(express.json());
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve favicon
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 // API route
 app.use('/api/generate', require('./api/generate'));
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
